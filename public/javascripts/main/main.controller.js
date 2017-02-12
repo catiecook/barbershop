321a41899eb31f0da71645c67d484b26713f286b
catiecook <catiecook888@gmail.com>
2017-02-11 17:17:19 -0700

    initial

--- public/javascripts/main/main.controller.js
+++ public/javascripts/main/main.controller.js
@@ -1,14 +1,14 @@
 angular.module('gif')
 
-.controller('MainController', function($scope, mainService, $timeout) {
+.controller('MainController', function($scope, mainService, $timeout, $mdDialog) {
     $scope.tempGif = "";
-    $scope.allGifs = [];
+    $scope.allGifs = ["https://media3.giphy.com/media/HVr4gFHYIqeti/giphy-downsized.gif"];
     $scope.start = true;
     $scope.end = false;
     $scope.first = false;
     $scope.counter = 0;
     $scope.points = 0;
-
+    let count = 0;
     $scope.conditionals = {
       puppy: false,
       boulder: false,
@@ -18,10 +18,16 @@ angular.module('gif')
       keep: false,
       leave: false,
       shiny: false,
-      free: false,
-      craigslist: false,
+      north: false,
       stay: false,
-      eat: false
+      eat: false,
+      pawn: false,
+      keep: false,
+      coffee: false,
+      end: false,
+      spaceship: false,
+      find: false,
+      find: false
     };
 
     $scope.start = function(cont) {
@@ -33,6 +39,12 @@ angular.module('gif')
       if(answer === "no") {
         $scope.start = false;
         $scope.end = true;
+        mainService.getGif("whatever")
+        .then((data) => {
+          let num = Math.floor(Math.random() * 10)
+          $scope.tempGif = data.data[num].images.downsized.url;
+          $scope.$apply();
+        })
       }
     };
 
@@ -62,7 +74,6 @@ angular.module('gif')
         case "west":
           $scope.conditionals.west = true;
           term = "cave";
-
           break;
         case "eat":
           $scope.conditionals.eat = true;
@@ -71,9 +82,8 @@ angular.module('gif')
           break;
         case "keep":
           $scope.conditionals.keep = true;
-          // $scope.conditionals.puppy = false;
           $scope.points += 10;
-          term = "fluffy puppy"
+          term = "puppy woods"
           break;
         case "leave":
           $scope.conditionals.leave = true;
@@ -83,25 +93,67 @@ angular.module('gif')
         case "shiny":
           $scope.conditionals.shiny = true;
           $scope.points += 30;
+          term = "treasure summer money speed cash bros";
           break;
         case "stay":
           $scope.conditionals.stay = true;
           $scope.points += 5;
-          term = "sunbathe"
+          term = "space abducted alien"
+          break;
+        case "pawn":
+          $scope.conditionals.pawn = true;
+          $scope.points += 5;
+          term = "pawn shop"
+          break;
+        case "coffee":
+          $scope.conditionals.coffee = true;
+          $scope.points += 5;
+          break;
+        case "beer":
+          $scope.conditionals.beer = true;
+          $scope.points += 5;
+          break;
+        case "north":
+          $scope.conditionals.north = true;
+          $scope.points += 5;
+          term = "city"
+          break;
+        case "free":
+          $scope.conditionals.leave = true;
+          $scope.points += 5;
+          term = "puppy run free"
+          break;
+        case "spaceship":
+          $scope.conditionals.spaceship = true;
+          $scope.points += 30;
+          break;
+        case "stairs":
+          $scope.conditionals.stairs = true;
+          $scope.points += 10;
+          break;
+        case "find":
+          $scope.conditionals.find = true;
+          $scope.points += 12;
+          term = "detective sherlock"
           break;
       }
-
 
       $scope.first = false;
 
       mainService.getGif(term)
       .then((data) => {
-        $scope.tempGif = data.data[0].images.downsized.url;
-        $scope.$apply();
-        $scope.allGifs.push({
-          1: data.data[0].images.downsized.url
-        });
-
+        $('.input').val("")
+        if (term === "treasure summer money speed cash bros"){
+          $scope.tempGif = data.data[0].images.downsized.url;
+          $scope.$apply();
+          $scope.allGifs.push(data.data[0].images.downsized.url);
+        }
+        else {
+            let num = Math.floor(Math.random() * 10)
+            $scope.tempGif = data.data[num].images.downsized.url;
+            $scope.$apply();
+            $scope.allGifs.push(data.data[num].images.downsized.url);
+        }
       });
     }
 });
