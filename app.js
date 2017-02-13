a7bf1ef8977e6f1160b82249745bbc1c1b2b1563
catiecook <catiecook888@gmail.com>
2017-02-13 08:45:51 -0700

    all partials mad ebut not linked

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-let express = require('express');
-let path = require('path');
-let favicon = require('serve-favicon');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-let routes = require('./routes/index');
+const routes = require('./routes/index');
 
-let app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
